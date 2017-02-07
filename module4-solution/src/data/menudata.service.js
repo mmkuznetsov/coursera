b3bcb0e799dedbdb1fb6832ae50d0b83f0a10a5b
Mike <mike@internet.com>
2017-02-06 21:07:18 -0600

    Module4 solution partial

---
+++ module4-solution/src/data/menudata.service.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+angular.module('Data')
+.service('MenuDataService', MenuDataService)
+.constant('ApiPath1', "https://davids-restaurant.herokuapp.com/categories.json"
+.constant('ApiPath2', "https://davids-restaurant.herokuapp.com/menu_items.json?category=");
+
+
+MenuDataService.$inject = ['$http', 'ApiPath1', 'ApiPath2']
+function MenuDataService('$http', 'ApiPath1', 'ApiPath2') {
+  var service = this;
+
+    service.getAllCategories = function () {
+        return $http({
+            method: "GET",
+            url: ApiPath1,
+        })
+        .then(function (result) {
+            return result;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    };
+    
+    service.getMatchedMenuItems = function (categoryShortName) {
+        return $http({
+            method: "GET",
+            url: (ApiPath2  + categoryShortName),
+            })
+        .then(function (result) {
+            return result;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    };
+}
+
+})();
