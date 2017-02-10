37fb9de7790afb4b2015d241116f7e41eecad82d
Mike <mike@internet.com>
2017-02-09 19:01:57 -0600

    Module4 solution with categories and second module

--- module4-solution/src/data/menudata.service.js
+++ module4-solution/src/data/menudata.service.js
@@ -3,12 +3,12 @@
 
 angular.module('Data')
 .service('MenuDataService', MenuDataService)
-.constant('ApiPath1', "https://davids-restaurant.herokuapp.com/categories.json"
+.constant('ApiPath1', "https://davids-restaurant.herokuapp.com/categories.json")
 .constant('ApiPath2', "https://davids-restaurant.herokuapp.com/menu_items.json?category=");
 
 
 MenuDataService.$inject = ['$http', 'ApiPath1', 'ApiPath2']
-function MenuDataService('$http', 'ApiPath1', 'ApiPath2') {
+function MenuDataService($http, ApiPath1, ApiPath2) {
   var service = this;
 
     service.getAllCategories = function () {
@@ -17,7 +17,7 @@ function MenuDataService('$http', 'ApiPath1', 'ApiPath2') {
             url: ApiPath1,
         })
         .then(function (result) {
-            return result;
+            return result.data;
         })
         .catch(function (error) {
             console.log(error);
